10cbe4028205873cd6513a794d650c3769e6fd7d
Adhithyan H Nair <133254493+Adhithyan-H-Nair@users.noreply.github.com>
2023-11-20 21:11:59 +0530

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,10 +1,17 @@
 window.addEventListener('load', function() {
-    if (document.documentElement.style.backgroundColor === 'black') {
-      document.querySelector('.loader').style.display = 'block';
-    } else {
-      document.querySelector('.loader').style.display = 'none';
-    }
-  });
+  // Hide the loader after the page is fully loaded
+  document.querySelector('.loader').style.display = 'none';
+
+  // Restore the background color to its original value
+  document.body.style.backgroundColor = '';
+});
+
+// Display the loader and change the background color to black when the page starts loading
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.loader').style.display = 'block';
+  document.body.style.backgroundColor = 'black';
+});
+
   
   window.addEventListener('load', function() {
     // Check if the user is viewing the site on a mobile device
